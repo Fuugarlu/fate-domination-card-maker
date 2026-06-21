a3642de176b52fc7c6f0cdccb0dd8d8d1f9866ca
Fuugarlu <mabinour@gmail.com>
2026-06-21 05:14:55 +0300

    Edit GH Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: '/fate-domination-card-maker/',
+  basePath: '/fate-domination-card-maker' ,
 };
 
 export default nextConfig;
