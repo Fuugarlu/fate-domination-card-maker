7c6a956dbeb06cfc2984893e7ffbcc70bde7036e
Fuugarlu <mabinour@gmail.com>
2026-09-09 16:28:42 +0300

    I accidentally clicked "Clear All" once so I'm changing that

--- src/components/header/ClearFormButton.tsx
+++ src/components/header/ClearFormButton.tsx
@@ -1,19 +1,34 @@
 import { formInput } from "@/src/features/master-card/types/formTypes";
-import React from "react";
+import React, { useState } from "react";
 
 type ClearFormButtonProps<T> = {
   setForm: React.Dispatch<React.SetStateAction<T>>;
   emptyState: T;
 };
 
-export const ClearFormButton = <T,>({setForm, emptyState}: ClearFormButtonProps<T>) => {
+export const ClearFormButton = <T,>({
+  setForm,
+  emptyState,
+}: ClearFormButtonProps<T>) => {
+  const [confirmationMessage, setConfirmationMessage] =
+    useState<boolean>(false);
+
+  const handleButtonPress = () => {
+    if (confirmationMessage == true) {
+      setForm(emptyState);
+      setConfirmationMessage(false);
+    } else {
+      setConfirmationMessage(true);
+    }
+  };
+
   return (
     <div>
       <button
-        onClick={() => setForm(emptyState)}
+        onClick={() => handleButtonPress()}
         className="bg-gray-700 import-export-button hover:bg-red-800 transition"
       >
-        CLEAR ALL
+        {confirmationMessage ? "REALLY CLEAR?" : "CLEAR ALL"}
       </button>
     </div>
   );
